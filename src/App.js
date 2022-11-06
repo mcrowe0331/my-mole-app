f808dc0d564242ea92b1d6fb5d874cc20a9d57c0
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-11-05 22:53:44 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import './App.css';
 import { useState } from 'react'
 import MoleContainer from './components/MoleContainer'
+import React from 'react';
+import './App.css';
+
+import mole from './mole.png';
 
 function App() {
   let [score, setScore] = useState(0)
 
   const createMoleHill = () => {
     let hills = []
-    for(let i = 0; i < 9; i++) {
-      hills.push(<MoleContainer key={i} setScore={setScore} score={score} />)
-    }
-    return (
-      <div>
-        { hills }
-      </div>
-    )
+    for (let i = 0; i < 9; i++) {
+        hills.push(
+          <MoleContainer key={i}
+          setScore={setScore}
+          score={score} />
+        )
   }
-  
+  return (
+    <div>
+      {hills}
+    </div>
+  )
+  }
   return (
     <div className="App">
-      <h1>React-A-Mole!</h1>
+       <h1>React-A-Mole!</h1>
       {score}
       {createMoleHill()}
     </div>
   );
 }
+  
 
 export default App;
